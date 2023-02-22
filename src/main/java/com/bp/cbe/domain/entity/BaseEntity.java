d52c077e40000e8c17ecc0f7ad1a2fd22b64416e
Bladimir Minga <bminga@pegasus.cl>
2023-02-21 22:11:17 -0500

    Refactor

--- src/main/java/com/bp/cbe/domain/entity/BaseEntity.java
+++ src/main/java/com/bp/cbe/domain/entity/BaseEntity.java
@@ -1,16 +1,11 @@
 package com.bp.cbe.domain.entity;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
 
 @MappedSuperclass
 @Data
@@ -18,10 +13,9 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 public class BaseEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@NotNull(message = "state no puede ser nulo")
-	@Column(name = "status", nullable = false)
-	private Boolean status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "status", nullable = false)
+    private Boolean status = Boolean.TRUE;
 }
